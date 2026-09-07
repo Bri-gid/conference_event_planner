ba8ffa5f710e5f2b3654df3d295ddd56fe578302
Bri-gid <brigidaddaimmra@gmail.com>
2026-09-07 09:25:23 -0400

    the second

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
+import avReducer from './avSLice';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReducer,
   },
 });
