2ac660c63ee9b8673acc4cf38da799cdfd6de9d0
Bri-gid <brigidaddaimmra@gmail.com>
2026-09-09 08:04:31 -0400

    add-ons

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -3,6 +3,7 @@ import "./ConferenceEvent.css";
 import TotalCost from "./TotalCost";
 import { useSelector, useDispatch } from "react-redux";
 import { incrementQuantity, decrementQuantity } from "./venueSlice";
+import {incrementAvQuantity,decrementAvQuantity} from "./avSlice";
 const ConferenceEvent = () => {
     const [showItems, setShowItems] = useState(false);
     const [numberOfPeople, setNumberOfPeople] = useState(1);
@@ -30,9 +31,11 @@ const ConferenceEvent = () => {
         }
       };
     const handleIncrementAvQuantity = (index) => {
+        dispatch(incrementAvQuantity(index));
     };
 
     const handleDecrementAvQuantity = (index) => {
+        dispatch(decrementAvQuantity(index));
     };
 
     const handleMealSelection = (index) => {
@@ -54,9 +57,16 @@ const ConferenceEvent = () => {
           venueItems.forEach((item) => {
             totalCost += item.cost * item.quantity;
           });
+        } else if (section ==="av"){
+            avItems.forEach((item) => {
+                totalCost +=item.cost * item.quantity;
+            });
         }
         return totalCost;
       };
+
+
+    const avTotalCost = calculateTotalCost("av");
     const venueTotalCost = calculateTotalCost("venue");
 
     const navigateToProducts = (idType) => {
@@ -158,6 +168,20 @@ const ConferenceEvent = () => {
 
                                 </div>
                                 <div className="addons_selection">
+                                    {avItems.map((item, index)=> (
+                                        <div className = "av_data venue_main" key={index}>
+                                            <div className="img">
+                                                <img src={item.img} alt= {item.name} />
+                                            </div> 
+                                        <div className ="text">{item.name} </div>
+                                        <div> ${item.cost} </div>
+                                            <div className ="addons_btn">
+                                                <button className = "btn-warninig" onClick= {() => handleDecrementAvQuantity(index)} > &ndash;</button>
+                                                <span className = "quantity-value">{item.quantity}</span>
+                                                <button className = "btn-success" onClick={()=> handleIncrementAvQuantity(index)}> &#43;</button>
+                                            </div>
+                                        </div>
+                                    ))}
                                     
 
                                 </div>
@@ -180,7 +204,7 @@ const ConferenceEvent = () => {
                                 <div className="meal_selection">
 
                                 </div>
-                                <div className="total_cost">Total Cost: </div>
+                                <div className="total_cost">Total Cost:{avTotalCost} </div>
 
 
                             </div>
